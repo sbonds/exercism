764099e563c5a3acb810a195d8ac190dc7a929aa
Steve Bonds <sbonds@gmail.com>
2019-02-13 21:30:40 -0800

    Say hello to the world

--- go/hello-world/hello_world.go
+++ go/hello-world/hello_world.go
@@ -1,15 +1,15 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
+/*
+Package greeting implements a function to say hello to something
 
-// Package greeting should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+why not the world?
+
+*/
 package greeting
 
-// HelloWorld should have a comment documenting it.
+/*
+Return "Hello, World!" back to the caller
+
+*/
 func HelloWorld() string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	return "Hello, World!"
 }
