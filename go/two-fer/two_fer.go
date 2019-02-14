90e19c37e2d22ceb841e239ab45d2f63f4e04f3b
Steve Bonds <sbonds@gmail.com>
2019-02-13 21:43:17 -0800

    Return a string, handling the case of name being empty.

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,15 +1,25 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
+/*
+Package twofer implements a function "ShareWith" that takes a name (string) as an
+argument. It should return the string:
 
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+	One for <name>, one for me.
+
+If the string is empty it should print:
+
+	One for you, one for me.
+
+*/
 package twofer
 
-// ShareWith should have a comment documenting it.
+import "fmt"
+
+/*
+ShareWith returns an appropriate response to the name passed in to it.
+
+*/
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	if name == "" {
+		name = "you"
+	}
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
