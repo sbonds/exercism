48402263d688d9b7d0bbf9468f8646a6491f319c
Steve Bonds <sbonds@gmail.com>
2019-02-18 14:16:14 -0800

    Switch not part of Strawberry Perl on Windows

--- perl5/bob/Bob.pm
+++ perl5/bob/Bob.pm
@@ -2,12 +2,22 @@
 package Bob;
 use strict;
 use warnings;
+use Switch;
 use Exporter 'import';
 our @EXPORT_OK = qw(hey);
 
 sub hey {
   my ($msg) = @_;
-  # Remove the comments and write some code here to pass the test suite.
+  # Use negative offsets to count from the end rather than the beginning of a string
+  my ($last_char) = substr($msg, -1);
+  # https://perldoc.perl.org/5.8.9/Switch.html
+  # Switch does not appear to be part of Strawberry Perl v5.28.1 even though it's listed
+  # as a core module.
+  switch ($last_char) {
+    case '?' { return "Sure."}
+    case '!' { return "Whoam chill out!"}
+
+  }
 }
 
 1;
