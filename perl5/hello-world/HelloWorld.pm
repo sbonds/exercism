8392786fb8a9bc2d825aa79d49aff1c14c52b56c
Steve Bonds <sbonds@gmail.com>
2019-02-18 13:36:49 -0800

    Perl exercise 'hello world' as downloaded
    Signed-off-by: Steve Bonds <sbonds@gmail.com>

---
+++ perl5/hello-world/HelloWorld.pm
@@ -0,0 +1,12 @@
+# Declare package 'HelloWorld'
+package HelloWorld;
+use strict;
+use warnings;
+use Exporter 'import';
+our @EXPORT_OK = qw(hello);
+
+sub hello {
+  # Remove the comments and write some code here to pass the test suite.
+}
+
+1;
