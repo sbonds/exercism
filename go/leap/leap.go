d823b37d5ef2c2f28ff2546a1f51648616c55693
Steve Bonds <sbonds@gmail.com>
2019-02-18 09:08:50 -0800

    Wow, first try!

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,15 +1,18 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package leap should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+/*Package leap determines if a given Gregorian year number is a leap year */
 package leap
 
-// IsLeapYear should have a comment documenting it.
-func IsLeapYear(int) bool {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return false
+/*IsLeapYear returns a boolean describing if the passed integer is a
+leap year by the following logic:
+
+	on every year that is evenly divisible by 4
+		except every year that is evenly divisible by 100
+			unless the year is also evenly divisible by 400
+
+This means a year is a leap year if:
+
+Year evenly divides by 4 AND (year is not evenly divisible by 100
+OR year is divisible by 400)
+*/
+func IsLeapYear(year int) bool {
+	return ((year%4 == 0) && ((year%100 != 0) || (year%400 == 0)))
 }
