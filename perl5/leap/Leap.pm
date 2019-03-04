ab0c5c84a17dd5835db2238acc88ec42176a9c62
Steve Bonds <sbonds@gmail.com>
2019-03-04 12:10:27 -0800

    Perl5 exercise 'leap' as downloaded
    Signed-off-by: Steve Bonds <sbonds@gmail.com>

---
+++ perl5/leap/Leap.pm
@@ -0,0 +1,14 @@
+# Declare package 'Leap'
+package Leap;
+use strict;
+use warnings;
+use Exporter 'import';
+our @EXPORT_OK = qw(is_leap_year);
+
+sub is_leap_year {
+  my ($year) = @_;
+  return undef;
+  # Remove the comments and write some code here to pass the test suite.
+}
+
+1;
