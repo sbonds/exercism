5920f0deb986bb0d591da2f01ed2ab47bc06c5a0
Steve Bonds <sbonds@gmail.com>
2019-03-04 07:58:41 -0800

    Revert needless 'optimization' to simpler code.
    Signed-off-by: Steve Bonds <sbonds@gmail.com>

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -3,6 +3,8 @@ the Scrabble game
 */
 package scrabble
 
+import "unicode"
+
 /* The value for each letter. We could have made this more compact by
 making arrays of letters and searching them to find the values, but the
 size of a 26 element list isn't too bad and it's extremely straightforward
@@ -45,32 +47,6 @@ var letterScore = map[rune]int{
 	'X': 8,
 	'Y': 4,
 	'Z': 10,
-	'a': 1,
-	'b': 3,
-	'c': 3,
-	'd': 2,
-	'e': 1,
-	'f': 4,
-	'g': 2,
-	'h': 4,
-	'i': 1,
-	'j': 8,
-	'k': 5,
-	'l': 1,
-	'm': 3,
-	'n': 1,
-	'o': 1,
-	'p': 3,
-	'q': 10,
-	'r': 1,
-	's': 1,
-	't': 1,
-	'u': 1,
-	'v': 4,
-	'w': 4,
-	'x': 8,
-	'y': 4,
-	'z': 10,
 }
 
 /*Score returns the Scrabble value of a word (string) provided to it.
@@ -84,9 +60,11 @@ func Score(originalWord string) int {
 	// Also helpful: https://blog.golang.org/strings
 	// https://stackoverflow.com/questions/18130859/how-can-i-iterate-over-a-string-by-runes-in-go
 	for _, char := range originalWord {
+		// Make the input uppercase to simplify the map we need to make
+		uppercaseChar := unicode.ToUpper(char)
 		// It might be smart to put a handler in here for the case of letterScore not
 		// existing for a given byte
-		wordScore += letterScore[char]
+		wordScore += letterScore[uppercaseChar]
 	}
 	return wordScore
 }
@@ -115,16 +93,4 @@ Using unicode.ToUpper inside the loop instead of converting the word ahead of ti
 	PASS
 	ok      scrabble-score    3.548s
 
-Making the map larger to do a direct lookup whether uppercase or lowercase:
-
-	$ go test -v --bench . --benchmem
-	=== RUN   TestScore
-	--- PASS: TestScore (0.00s)
-	goos: windows
-	goarch: amd64
-	BenchmarkScore-4         2000000               855 ns/op               0 B/op         0 allocs/op
-	PASS
-	ok      scrabble-score    2.926s
-
-Essentially no change-- a needless "optimization"
 */
