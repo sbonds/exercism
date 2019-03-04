08d65a209d16d466e8410e1c4e18c7775edcc78b
Steve Bonds <sbonds@gmail.com>
2019-03-04 12:15:34 -0800

    Used the same logic from the Go version

--- perl5/leap/Leap.pm
+++ perl5/leap/Leap.pm
@@ -5,10 +5,20 @@ use warnings;
 use Exporter 'import';
 our @EXPORT_OK = qw(is_leap_year);
 
+# is_leap_year returns a boolean describing if the passed integer is a
+# leap year by the following logic:
+#
+#	on every year that is evenly divisible by 4
+#		except every year that is evenly divisible by 100
+#			unless the year is also evenly divisible by 400
+#
+#This means a year is a leap year if:
+#
+#Year evenly divides by 4 AND (year is not evenly divisible by 100
+#OR year is divisible by 400)
 sub is_leap_year {
   my ($year) = @_;
-  return undef;
-  # Remove the comments and write some code here to pass the test suite.
+  return (($year % 4 == 0) && (($year % 100 != 0) || ($year % 400 == 0)));
 }
 
 1;
