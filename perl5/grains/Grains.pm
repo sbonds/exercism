5780c6b20988c3b08ae2ceec7b183a17b367b547
Steve Bonds <sbonds@gmail.com>
2019-03-04 12:17:17 -0800

    Perl5 exercise 'grains' as downloaded
    Signed-off-by: Steve Bonds <sbonds@gmail.com>

---
+++ perl5/grains/Grains.pm
@@ -0,0 +1,16 @@
+package Grains;
+use strict;
+use warnings;
+use Exporter 'import';
+our @EXPORT_OK = qw(grains_on_square total_grains);
+
+sub grains_on_square {
+  my ($square) = @_;
+  return undef;
+}
+
+sub total_grains {
+  return undef;
+}
+
+1;
