0aa4e5ae095dab98bf253262bc375920e163a3bb
Steve Bonds <sbonds@gmail.com>
2019-03-08 11:55:31 -0800

    Tests pass

--- perl5/hamming/Hamming.pm
+++ perl5/hamming/Hamming.pm
@@ -6,7 +6,14 @@ our @EXPORT_OK = qw(hamming_distance);
 
 sub hamming_distance {
   my ($strand1, $strand2) = @_;
-  return undef;
+  unless ( length($strand1) == length($strand2) ) { die "The two strands must have equal length"}
+  my $hamming_distance = 0;
+  for (my $i = 0; $i < length($strand1); $i++) {
+    if (substr($strand1,$i,1) ne substr($strand2,$i,1)) { 
+      $hamming_distance++
+    }
+  }
+  return $hamming_distance;
 }
 
 1;
