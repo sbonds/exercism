2e668a89b18bae4e70e657aa64ac0747226438d8
Steve Bonds <sbonds@gmail.com>
2019-03-09 07:20:40 -0800

    Basic framework in place

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,11 @@
+=begin
+Given a number, return a string based on whether 3, 5, or 7 are factors.
+
+=end
+
+class Raindrops
+  # I can see no use of new() in the tests so I know I'll need a class method. I'm learning! (*looks amazed*)
+  def self.convert(number)
+    return nil
+  end
+end
