83a4f10b46f5263d2004d6557ae2e881f2f1b5f8
Steve Bonds <sbonds@gmail.com>
2019-03-09 10:02:12 -0800

    Passes all tests

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -23,6 +23,23 @@ class Isogram
     # Well... that's not a good start. The regex method relies on a positive match for something in order to return
     # and a null string doesn't have any repeating characters, but also won't itself repeat. The test only works if
     # there's at least one word character in the string.
-    word.scan(/\w/) ? true : word.scan(/(\w).*\1/).length > 0
+    #
+    # word.scan(/\w/) ? true : word.scan(/(\w).*\1/i).length > 0
+    #
+    #   1) Failure:
+    #   IsogramTest#test_hypothetical_word_with_duplicated_character_following_hyphen [isogram_test.rb:57]:
+    #   Expected false, 'thumbscrew-jappingly' is not an isogram
+    # 
+    # I would expect the '*' after '.' to mean that zero or more of any characters would be there.
+    # Since that includes zero, it seems odd that the above fails. Test in IRB...
+    #
+    #   >> "lumberjacks".scan(/(\w).*\1/).length >0
+    #   => false
+    #   >> "lumberrjacks".scan(/(\w).*\1/).length >0
+    #   => true
+    # 
+    # OK, that's what I would expect, so why is "jappingly" not working?
+    # /facepalm... I had my logic completely reversed in my brain. Whoops! Another good case for test-driven development!
+    word.scan(/\w/) ? word.scan(/(\w).*\1/i).length.zero? : true
   end
 end
