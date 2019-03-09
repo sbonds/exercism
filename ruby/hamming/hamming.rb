c4ec437f2ce82fad4d38fd7b1e963703933d2c03
Steve Bonds <sbonds@gmail.com>
2019-03-09 05:34:08 -0800

    Learned something about parameter passing

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -56,8 +56,74 @@ class Hamming
     #   => "GATTACA"
     # Hey, that's pretty close for so early in the IRB process! Add a comparison and some summing up to count 
     # the differences.
-    distance = 0
-    first_strand.each_char.each_with_index{ |nucleotide, position| distance += 1 unless ( nucleotide == second_strand[position] ) }
-    return distance
+    #
+    # Mentor ajoshguy reminded me about the joy of .count vs. a manual count and suggested that since each_char already
+    # iterates that each_with_index would be overkill. Look at .with_index instead.
+    # IRB time!
+    #   >> first_strand="GATTACA";second_strand="GATTACG";first_strand.each_char.with_index{ 
+    #      |nucleotide, position| printf("%s:%s\n", position, nucleotide) }
+    #   0:G
+    #   1:A
+    #   2:T
+    #   3:T
+    #   4:A
+    #   5:C
+    #   6:A
+    #   => "GATTACA"
+    # So yeah, with_index is an identical result. So... now to figure out how to count only the non-matching
+    # nucleotides via collections. https://ruby-doc.org/core-2.5.3/Enumerable.html#method-i-count
+    # Oh, hey! There's a count{ |obj| block } method. Maybe that would be helpful for filtering only the ones I want?
+    # But where to put it? I can't put it inside the with_index... can I?
+    # No, but checking the Enumerator (not Enumerable) docs shows that with_index without a block creates a new Enumerator
+    # that includes the index with each enumerated item. https://ruby-doc.org/core-2.5.3/Enumerator.html#method-i-with_index
+    # Maybe the object passed into "count" after with_index has a way to pull the index?
+    #   >> first_strand.each_char.with_index.count{ |i| p i}
+    #   "G"
+    #   "A"
+    #   "T"
+    #   "T"
+    #   "A"
+    #   "C"
+    #   "A"
+    #   => 7
+    #   >> first_strand.each_char.with_index.count{ |i| p i.class}
+    #   String
+    #   String
+    #   String
+    #   String
+    #   String
+    #   String
+    #   String
+    #   => 7
+    # I don't see anything useful passed into count via the |obj|. A simple string comes in, and it has no index info.
+    # As a total long shot (not mentioned anywhere in the docs) try reading TWO objects from the enumerator from count
+    #   >> first_strand.each_char.with_index.count{ |i, j| p i,j}
+    #   "G"
+    #   0
+    #   "A"
+    #   1
+    #   "T"
+    #   2
+    #   "T"
+    #   3
+    #   "A"
+    #   4
+    #   "C"
+    #   5
+    #   "A"
+    #   6
+    #   => 7
+    # I did not expect that guess to work, but hey, I never know until I try!
+    # Finish it out with a test inside count...
+    #   >> p first_strand
+    #   "GATTACA"
+    #   => "GATTACA"
+    #   >> p second_strand
+    #   "GATTACG"
+    #   => "GATTACG"
+    #   >> first_strand.each_char.with_index.count{ |nucleotide, position| nucleotide != second_strand[position] }
+    #   => 1
+    # Looks promising!
+    first_strand.each_char.with_index.count{ |nucleotide, position| nucleotide != second_strand[position] }
   end
 end
