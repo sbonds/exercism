85b6dc11ed453cff6bb811bb4e157854e3551ba6
Steve Bonds <sbonds@gmail.com>
2019-03-09 20:17:44 -0800

    Make our letter table a constant.

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -16,38 +16,40 @@ class Scrabble
     dirty_word ? dirty_word.downcase.scan(/[a-z]/).join : ""
   end
 
+  # The word should be normalized so we can index by lowercase letters only. Scrable scores don't really follow
+  # much in the way of patterns, so a lookup for each letter seems justified. The other way to do this would be
+  # to index by score with a list of letters matching that score. That seems to make the lookup more difficult.
+  LETTER_SCORE_FOR = {
+    'a' => 1,
+    'b' => 3,
+    'c' => 3,
+    'd' => 2,
+    'e' => 1,
+    'f' => 4,
+    'g' => 2,
+    'h' => 4,
+    'i' => 1,
+    'j' => 8,
+    'k' => 5,
+    'l' => 1,
+    'm' => 3,
+    'n' => 1,
+    'o' => 1,
+    'p' => 3,
+    'q' => 10,
+    'r' => 1,
+    's' => 1,
+    't' => 1,
+    'u' => 1,
+    'v' => 4,
+    'w' => 4,
+    'x' => 8,
+    'y' => 4,
+    'z' => 10,
+  }.freeze
+
   def score
-    # The word should be normalized so we can index by lowercase letters only. Scrable scores don't really follow
-    # much in the way of patterns, so a lookup for each letter seems justified. The other way to do this would be
-    # to index by score with a list of letters matching that score. That seems to make the lookup more difficult.
-    letter_score_for = {
-      'a' => 1,
-      'b' => 3,
-      'c' => 3,
-      'd' => 2,
-      'e' => 1,
-      'f' => 4,
-      'g' => 2,
-      'h' => 4,
-      'i' => 1,
-      'j' => 8,
-      'k' => 5,
-      'l' => 1,
-      'm' => 3,
-      'n' => 1,
-      'o' => 1,
-      'p' => 3,
-      'q' => 10,
-      'r' => 1,
-      's' => 1,
-      't' => 1,
-      'u' => 1,
-      'v' => 4,
-      'w' => 4,
-      'x' => 8,
-      'y' => 4,
-      'z' => 10,
-    }
+  
     # I didn't see a way to do something like each_with_object to eliminate the need for a var outside the
     # main logic.
     # 
@@ -59,7 +61,7 @@ class Scrabble
     # 
     # Ah, they're not calling new() like I expected based on the first tests. Let me toss in a "convenient" class method.
     word_score = 0
-    @word.each_char{ |letter| word_score += letter_score_for[letter] }
+    @word.each_char{ |letter| word_score += LETTER_SCORE_FOR[letter] }
     return word_score
   end
 
