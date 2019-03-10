e3dae04160632e72941fe1bc101171b0901a2b7f
Steve Bonds <sbonds@gmail.com>
2019-03-10 07:53:36 -0700

    Passes first test

---
+++ c/isogram/src/isogram.c
@@ -0,0 +1,39 @@
+#include "isogram.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+
+/* I'll need some place to keep track of the letters which have already been seen and a way to check
+them later. But how can I know how big the phrase will be so I can make a buffer large enough?
+
+For now, define a fixed buffer than can handle a-z.
+
+*/
+
+#define MAX_PHRASE_SIZE 26
+
+bool is_isogram(const char phrase[]) 
+{
+  int i;
+  /* Index used: a=0, b=1, c=2, ... z=26
+    Also initialize to "false"
+  */
+  bool letter_seen[MAX_PHRASE_SIZE];
+  for(i=0;i<MAX_PHRASE_SIZE;i++)
+  {
+    letter_seen[i] = false;
+  }
+
+  int phrase_length=strlen(phrase);
+  for(i=0;i<phrase_length;i++)
+  {
+    printf("%c", phrase[i]);
+    int char_code=(int) tolower(phrase[i]);
+    /* TODO: put in a bounds check to prevent buffer overflow */
+    if (letter_seen[char_code] == true) {
+      return false;
+    }
+  }
+
+  return true;
+}
