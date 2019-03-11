4e70d5df2432c550b4d029157fc1e14e920f1448
Steve Bonds <sbonds@gmail.com>
2019-03-10 20:23:38 -0700

    Passes all tests now

--- perl5/word-count/Phrase.pm
+++ perl5/word-count/Phrase.pm
@@ -3,8 +3,11 @@ use strict;
 use warnings;
 use Data::Dumper;
 use Exporter 'import';
-our @EXPORT_OK = qw(new score);
+our @EXPORT_OK = qw(new word_count);
 
+our $string_of_words = "";
+
+# Gets passed a string containing words. Expects case to be normalized and punctuation to be ignored.
 sub new {
   my ($class, @args) = @_;
   my ($self) = {};
@@ -12,6 +15,22 @@ sub new {
   return $self;
 }
 
+# Return a hash of words as keys and the number of times as values.
 sub word_count {
-  return undef;
+  #print STDERR Data::Dumper->Dump([\@_],[qw(@_)]);
+  $string_of_words = shift;
+  #print STDERR Data::Dumper->Dump([\$string_of_words],[qw($string_of_words)]);
+  my %count_of;
+  # Make lowercase
+  $string_of_words =~ tr/A-Z/a-z/;
+  # Filter out non-word characters, replacing with whitespace
+  $string_of_words =~ s/\W/ /g;
+  
+  foreach my $word (split(/\s+/,$string_of_words)) {
+    $count_of{$word}++;
+  }
+  #print STDERR Data::Dumper->Dump([\%count_of],[qw(%count_of)]);
+  return \%count_of;
 }
+
+1;
