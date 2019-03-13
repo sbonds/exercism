0523eaba8688a3aced19bd0ccef84cbafe80c8fd
Steve Bonds <sbonds@gmail.com>
2019-03-13 15:34:51 -0700

    Mentor F3PiX wants more methods. Go nuts!

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,7 +1,31 @@
 class Luhn
+  def initialize(string_of_numbers)
+    @string_of_numbers = string_of_numbers
+  end
+
   def self.valid?(string_of_numbers)
-    return false if string_of_numbers.scan(/\d/).length < 2
-    return false unless string_of_numbers.match?(/^[\d\s]+$/)
-    string_of_numbers.reverse.scan(/\d/).map(&:to_i).map.with_index{ |number, index| index % 2 == 1 ? (number * 2 > 9 ? number*2 - 9: number*2) : number}.sum % 10 == 0
+    luhn = Luhn.new(string_of_numbers)
+    luhn.valid?
+  end
+
+  def long_enough?
+    @string_of_numbers.scan(/\d/).length > 1
+  end
+
+  def only_numbers?
+    @string_of_numbers.match?(/^[\d\s]+$/)
+  end
+
+  def early_checks_fine?
+    long_enough? && only_numbers?
+  end
+
+  def handle_over_nine(number)
+    number * 2 > 9 ? number*2 - 9: number*2
+  end
+
+  def valid?
+    return false unless early_checks_fine?
+    @string_of_numbers.reverse.scan(/\d/).map(&:to_i).map.with_index{ |number, index| index % 2 == 1 ? handle_over_nine(number) : number}.sum % 10 == 0
   end
 end
