6d8f1ac826d72c194ed7fb6f1099d89c89328c59
Steve Bonds <sbonds@gmail.com>
2019-03-19 08:01:36 -0700

    Using a Proc isn't making sense yet

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,4 +1,6 @@
 class Luhn
+  attr_reader :string_of_numbers
+
   def initialize(string_of_numbers)
     @string_of_numbers = string_of_numbers
   end
@@ -9,11 +11,11 @@ class Luhn
   end
 
   def long_enough?
-    @string_of_numbers.scan(/\d/).length > 1
+    string_of_numbers.scan(/\d/).length > 1
   end
 
   def only_numbers?
-    @string_of_numbers.match?(/^[\d\s]+$/)
+    string_of_numbers.match?(/^[\d\s]+$/)
   end
 
   def double_with_overflow(two_digits)
@@ -23,6 +25,10 @@ class Luhn
 
   def valid?
     return false unless ( long_enough? && only_numbers? )
-    @string_of_numbers.reverse.scan(/\d/).map(&:to_i).map.each_slice(2).map{ |up_to_two_digits| double_with_overflow(up_to_two_digits)}.sum % 10 == 0
+    # It seems like there should be a way to use &: or & to shorten that map{ || } block, but I'm not finding how to do that.
+    # Would I really need to extend Array to make that work?
+    #  map(&:double_with_overflow) gives:
+    #   NoMethodError: undefined method `double_with_overflow' for [9, 5]:Array
+    string_of_numbers.reverse.scan(/\d/).map(&:to_i).map.each_slice(2).map{ |up_to_two_digits| double_with_overflow(up_to_two_digits)}.sum % 10 == 0
   end
 end
