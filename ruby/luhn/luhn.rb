5c8833908eae2d01f5ae7088b43ec32b5686076f
Steve Bonds <sbonds@gmail.com>
2019-03-19 07:25:17 -0700

    Still passes, reducing methods and intermediate calculations

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -17,19 +17,12 @@ class Luhn
   end
 
   def double_with_overflow(two_digits)
-    p two_digits
     doubled_digit = (two_digits[1] || 0) * 2
-    doubled_digit = doubled_digit > 9 ? doubled_digit - 9: doubled_digit
-    puts "Returning #{doubled_digit} + #{two_digits[0]}"
-    doubled_digit + two_digits[0]
-  end
-
-  def reversed_digits_only
-    @string_of_numbers.reverse.scan(/\d/).map(&:to_i).map
+    (doubled_digit > 9 ? doubled_digit - 9: doubled_digit) + two_digits[0]
   end
 
   def valid?
     return false unless ( long_enough? && only_numbers? )
-    reversed_digits_only.each_slice(2).map{ |up_to_two_digits| double_with_overflow(up_to_two_digits)}.sum % 10 == 0
+    @string_of_numbers.reverse.scan(/\d/).map(&:to_i).map.each_slice(2).map{ |up_to_two_digits| double_with_overflow(up_to_two_digits)}.sum % 10 == 0
   end
 end
