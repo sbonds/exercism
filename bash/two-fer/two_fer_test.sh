83237f549fb6152350f30759ba7e987c4fba3cd3
Steve Bonds <sbonds@gmail.com>
2019-03-20 12:31:55 -0700

    Passes all tests

--- bash/two-fer/two_fer_test.sh
+++ bash/two-fer/two_fer_test.sh
@@ -8,14 +8,14 @@
 }
 
 @test "a name given" {
-  skip
+  #skip
   run bash two_fer.sh Alice
   [ "$status" -eq 0 ]
   [ "$output" == "One for Alice, one for me." ]
 }
 
 @test "another name given" {
-  skip
+  #skip
   run bash two_fer.sh Bob
   [ "$status" -eq 0 ]
   [ "$output" == "One for Bob, one for me." ]
