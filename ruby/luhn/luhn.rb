cf4ba8cfd6629fd2521547818ea82965365230ca
Steve Bonds <sbonds@gmail.com>
2019-03-25 14:48:28 -0700

    Reworking for a new iteration

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -23,12 +23,16 @@ class Luhn
     (doubled_digit > 9 ? doubled_digit - 9: doubled_digit) + two_digits[0]
   end
 
+  def valid_luhn_sum?
+    string_of_numbers.reverse.scan(/\d/).map(&:to_i).map.each_slice(2).map{ |up_to_two_digits| double_with_overflow(up_to_two_digits)}.sum % 10 == 0
+  end
+
   def valid?
     return false unless ( long_enough? && only_numbers? )
     # It seems like there should be a way to use &: or & to shorten that map{ || } block, but I'm not finding how to do that.
     # Would I really need to extend Array to make that work?
     #  map(&:double_with_overflow) gives:
     #   NoMethodError: undefined method `double_with_overflow' for [9, 5]:Array
-    string_of_numbers.reverse.scan(/\d/).map(&:to_i).map.each_slice(2).map{ |up_to_two_digits| double_with_overflow(up_to_two_digits)}.sum % 10 == 0
+    valid_luhn_sum?
   end
 end
