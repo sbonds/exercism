b5cd11e2877806db4a884b352be4f17cd300bc30
Steve Bonds <sbonds@gmail.com>
2019-03-28 11:27:37 -0700

    Compiles but doesn't pass tests.

---
+++ go/luhn/luhn.go
@@ -0,0 +1,11 @@
+/*Package luhn determines if a given sequence of numbers matches the Luhn checksum .*/
+package luhn
+
+/*Based on the testing it looks like we need to implement a method called Valid(string)
+which returns a boolean based on whether the string contains a valid series of numbers
+matching the Luhn checksum.
+*/
+
+func Valid(sequence_of_numbers string) bool {
+	return false
+}
