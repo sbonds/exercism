e78e7b2df9a1cc29886697936d6462d484095012
Steve Bonds <sbonds@gmail.com>
2019-03-28 06:35:29 -0700

    Utility function solution: move to another class
    But probably not worth the complexity

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -17,6 +17,10 @@ class Luhn
 
   private 
 
+  # This 'utility function' makes no use of class variables so it could be located anywhere.
+  # For example, it could be moved to a new class. However, since it's a private method and
+  # adding a new class would be more complex than leaving this short function in place,
+  # this utility function seems the lesser of the two evils.
   def double_with_overflow(two_digits)
     doubled_digit = (two_digits[1] || 0) * 2
     (doubled_digit < 10 ? doubled_digit : doubled_digit - 9) + two_digits[0]
@@ -27,6 +31,8 @@ class Luhn
   end
 
   def luhn_sum
+    # It seems like sum { |m| method(m) } should be able to be coerced into sum { &:method } somehow.
+    # There seems to be some aspect of .to_proc which I'm missing that might enable this.
     string_of_numbers.reverse.chars.map(&:to_i).each_slice(2).sum{ |up_to_two_digits| double_with_overflow(up_to_two_digits)}
   end
 
