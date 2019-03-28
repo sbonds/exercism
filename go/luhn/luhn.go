4534bd6b3279cce0bba9369a98e827c7ecc05a63
Steve Bonds <sbonds@gmail.com>
2019-03-28 11:48:17 -0700

    Passes some tests and golint

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,11 +1,26 @@
 /*Package luhn determines if a given sequence of numbers matches the Luhn checksum .*/
 package luhn
 
+import "strings"
+
 /*Based on the testing it looks like we need to implement a method called Valid(string)
 which returns a boolean based on whether the string contains a valid series of numbers
 matching the Luhn checksum.
 */
 
-func Valid(sequence_of_numbers string) bool {
-	return false
+/*Valid returns true if the digits in the input make a valid Luhn checksum, false otherwise. */
+func Valid(sequenceOfNumbers string) bool {
+	spaceFreeSequenceOfNumbers := RemoveSpaces(sequenceOfNumbers)
+	if len(spaceFreeSequenceOfNumbers) <= 1 {
+		return false
+	}
+	return true
+}
+
+/*RemoveSpaces is used internally to remove any spaces from the string passed in.
+It could also have been 'unexported' based on golint, but that seemed to be surprisingly
+unusual and/or difficult to do. */
+func RemoveSpaces(stringWithSpaces string) string {
+	// Split on spaces and join on nothing. It's simple to read, but may not benchmark well.
+	return strings.Join(strings.Fields(stringWithSpaces), "")
 }
