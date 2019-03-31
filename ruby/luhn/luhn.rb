f557cb49803100cb330402b5c7a251ed89a92537
Steve Bonds <sbonds@gmail.com>
2019-03-31 06:35:10 -0700

    Can split out the digits before passing them in

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -21,9 +21,9 @@ class Luhn
   # For example, it could be moved to a new class. However, since it's a private method and
   # adding a new class would be more complex than leaving this short function in place,
   # this utility function seems the lesser of the two evils.
-  def double_with_overflow(two_digits)
-    doubled_digit = (two_digits[1] || 0) * 2
-    (doubled_digit < 10 ? doubled_digit : doubled_digit - 9) + two_digits[0]
+  def double_with_overflow(first_digit, second_digit)
+    doubled_digit = (second_digit || 0) * 2
+    (doubled_digit < 10 ? doubled_digit : doubled_digit - 9) + first_digit
   end
 
   def valid_format?
@@ -33,7 +33,7 @@ class Luhn
   def luhn_sum
     # It seems like sum { |m| method(m) } should be able to be coerced into sum { &:method } somehow.
     # There seems to be some aspect of .to_proc which I'm missing that might enable this.
-    string_of_numbers.reverse.chars.map(&:to_i).each_slice(2).sum{ |up_to_two_digits| double_with_overflow(up_to_two_digits)}
+    string_of_numbers.reverse.chars.map(&:to_i).each_slice(2).sum{ |first_digit, second_digit| double_with_overflow(first_digit, second_digit)}
   end
 
   def valid_luhn_sum?
