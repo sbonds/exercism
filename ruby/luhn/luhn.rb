eb08cb8a85af3abacd01bdab197e71a52dfb9525
Steve Bonds <sbonds@gmail.com>
2019-03-31 06:56:15 -0700

    Other suggestios for iteration 8 done
    But the ternary is still there. Other options seemed to stink even more.

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -6,7 +6,6 @@ class Luhn
     Luhn.new(string_of_numbers).valid?
   end
 
-  # https://github.com/rubocop-hq/ruby-style-guide#consistent-classes
   def initialize(string_of_numbers)
     @string_of_numbers = string_of_numbers.delete(" ")
   end
@@ -17,10 +16,6 @@ class Luhn
 
   private
 
-  # This 'utility function' makes no use of class variables so it could be located anywhere.
-  # For example, it could be moved to a new class. However, since it's a private method and
-  # adding a new class would be more complex than leaving this short function in place,
-  # this utility function seems the lesser of the two evils.
   def double_with_overflow(digit)
     doubled_digit = (digit || 0) * 2
     doubled_digit < 10 ? doubled_digit : doubled_digit - 9
@@ -31,14 +26,12 @@ class Luhn
   end
 
   def luhn_sum
-    # It seems like sum { |m| method(m) } should be able to be coerced into sum { &:method } somehow.
-    # There seems to be some aspect of .to_proc which I'm missing that might enable this.
-    string_of_numbers.reverse.chars.map(&:to_i).each_slice(2).sum do |first_digit, second_digit| 
+    string_of_numbers.reverse.chars.map(&:to_i).each_slice(2).sum do |first_digit, second_digit|
       double_with_overflow(second_digit) + first_digit
     end
   end
 
   def valid_luhn_sum?
-     luhn_sum % 10 == 0
+    luhn_sum % 10 == 0
   end
 end
