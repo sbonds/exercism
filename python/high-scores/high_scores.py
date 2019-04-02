0ab9b0772206966eb8099edb0e70740f532396a7
Steve Bonds <sbonds@gmail.com>
2019-04-02 11:34:22 -0700

    Enabled format on save with autopep8

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -4,16 +4,17 @@
 # latest: returns the last member of the high scores array
 # personal_best: returns the largest score in the array
 # personal_top_three: returns an array of up to three elements containing the highest scores
+
+
 class HighScores(object):
     def __init__(self, scores):
-        self.scores=scores
+        self.scores = scores
 
     def personal_best(self):
         return max(self.scores)
 
     def personal_top_three(self):
-        return sorted(self.scores,reverse=True)[:3]
+        return sorted(self.scores, reverse=True)[:3]
 
     def latest(self):
         return self.scores[-1]
-
