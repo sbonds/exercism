824b19787ff868be80af769de0616b04dc35ec29
Steve Bonds <sbonds@gmail.com>
2019-04-11 10:20:42 -0700

    Mentor suggests that an empty name should succeed

--- bash/error-handling/error_handling.sh
+++ bash/error-handling/error_handling.sh
@@ -11,15 +11,9 @@ set -o nounset
 main() {
   greetee=${1:-}
 
-  # If there's nobody to greet, print appropriate error
-  if [ -z "$greetee" ]; then
-    echo "Usage: ./error_handling <greetee>"
-    exit 1
-  fi
-
   # If there are extra arguments, print appropriate error
   if [ "$#" -ne 1 ]; then
-    echo "Wrong number of arguments, should be exactly one"
+    echo "Usage: ./error_handling <greetee>"
     exit 1
   fi
 
