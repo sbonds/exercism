7e94070918a3e4ad48e52801c335f7cae62480be
Steve Bonds <sbonds@gmail.com>
2019-04-19 08:47:34 -0700

    Passes first test

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-# Convert a long name to an acronym.
+# Convert a series of words to an initialism.
+# Which method to use? So far I can think of:
+# + shell out to sed/awk (avoid that, this is a bash exercise!)
+# + split using read and an array
+# + split using regexp and BASH_REMATCH
 set -o errexit
 set -o nounset
 
@@ -9,6 +13,17 @@ main() {
     echo "Usage: $0 '<string>'"
     return 1
   fi
+  initialism=""
+
+  # Using read + array seems the best balance between simple,
+  # backwards compatible, and efficient.
+  read -a words <<< "$1" 
+
+  for word in "${words[@]}"; do
+    initialism="${initialism}${word:0:1}"
+  done
+
+  echo "$initialism"
 
   return 0
 }
