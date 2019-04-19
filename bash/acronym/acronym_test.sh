56ee4a5f388229429850009e63f1ab21ba62ba9e
Steve Bonds <sbonds@gmail.com>
2019-04-19 08:50:58 -0700

    Fails lowercase and embedded punctuation

--- bash/acronym/acronym_test.sh
+++ bash/acronym/acronym_test.sh
@@ -8,35 +8,35 @@
 }
 
 @test 'lowercase words' {
-  skip
+  #skip
   run bash acronym.sh 'Ruby on Rails'
   [ "$status" -eq 0 ]
   [ "$output" == 'ROR' ]
 }
 
 @test 'punctuation' {
-  skip
+  #skip
   run bash acronym.sh 'First In, First Out'
   [ "$status" -eq 0 ]
   [ "$output" == 'FIFO' ]
 }
 
 @test 'all caps word' {
-  skip
+  #skip
   run bash acronym.sh 'GNU Image Manipulation Program'
   [ "$status" -eq 0 ]
   [ "$output" == 'GIMP' ]
 }
 
 @test 'punctuation without whitespace' {
-  skip
+  #skip
   run bash acronym.sh 'Complementary metal-oxide semiconductor'
   [ "$status" -eq 0 ]
   [ "$output" == 'CMOS' ]
 }
 
 @test 'very long abbreviation' {
-  skip
+  #skip
   run bash acronym.sh 'Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me'
   [ "$status" -eq 0 ]
   [ "$output" == 'ROTFLSHTMDCOALM' ]
