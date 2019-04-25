e9cadc364f360d2c69c2e809417ab5a0f4d87b5a
Steve Bonds <sbonds@gmail.com>
2019-04-24 19:06:17 -0700

    Same script passes all tests

--- bash/armstrong-numbers/armstrong_numbers_test.sh
+++ bash/armstrong-numbers/armstrong_numbers_test.sh
@@ -9,7 +9,7 @@
 }
 
 @test 'There are no two digit Armstrong numbers' {
-  skip
+  # skip
   run bash armstrong_numbers.sh 10
 
   [ "$status" -eq 0 ]
@@ -17,7 +17,7 @@
 }
 
 @test 'A three digit number that is an Armstrong number' {
-  skip
+  # skip
   run bash armstrong_numbers.sh 153
 
   [ "$status" -eq 0 ]
@@ -25,7 +25,7 @@
 }
 
 @test 'A three digit number that is not an Armstrong number' {
-  skip
+  # skip
   run bash armstrong_numbers.sh 100
 
   [ "$status" -eq 0 ]
@@ -33,7 +33,7 @@
 }
 
 @test 'A four digit number that is an Armstrong number' {
-  skip
+  # skip
   run bash armstrong_numbers.sh 9474
 
   [ "$status" -eq 0 ]
@@ -41,7 +41,7 @@
 }
 
 @test 'A four digit number that is not an Armstrong number' {
-  skip
+  # skip
   run bash armstrong_numbers.sh 9475
 
   [ "$status" -eq 0 ]
@@ -49,7 +49,7 @@
 }
 
 @test 'A seven digit number that is an Armstrong number' {
-  skip
+  # skip
   run bash armstrong_numbers.sh 9926315
 
   [ "$status" -eq 0 ]
@@ -57,7 +57,7 @@
 }
 
 @test 'A seven digit number that is not an Armstrong number' {
-  skip
+  # skip
   run bash armstrong_numbers.sh 9926314
 
   [ "$status" -eq 0 ]
