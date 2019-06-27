5b40908598acc9a54b99731ea309da39fb9d95c5
Steve Bonds <sbonds@gmail.com>
2019-06-27 10:11:22 -0700

    WIP: left some uncommitted changes

---
+++ c/word-count/src/word_count.c
@@ -0,0 +1,10 @@
+#include "word_count.h"
+#include <string.h>
+
+int word_count(const char *input_text, word_count_word_t *words)
+{
+    words->text = "hello";
+    words->count = "42";
+    input_text = "";
+    return 1;
+}
