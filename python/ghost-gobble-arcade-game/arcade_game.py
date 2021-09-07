2f21bb8299fda96fff11d00e3522b36eb8f1e41d
Steve Bonds <steve.bonds@dxc.com>
2021-09-07 13:52:24 -0700

    Define the criteria for each function.

--- python/ghost-gobble-arcade-game/arcade_game.py
+++ python/ghost-gobble-arcade-game/arcade_game.py
@@ -1,9 +1,12 @@
+"""Exercism python ghost-gobble-arcade-game"""
 def eat_ghost(power_pellet_active, touching_ghost):
     """
 
     :param power_pellet_active: bool - does the player have an active power pellet?
     :param touching_ghost:  bool - is the player touching a ghost?
     :return: bool
+
+    The function should return True only if Pac-Man has a power pellet active and is touching a ghost.
     """
     pass
 
@@ -14,6 +17,8 @@ def score(touching_power_pellet, touching_dot):
     :param touching_power_pellet: bool - does the player have an active power pellet?
     :param touching_dot:  bool - is the player touching a dot?
     :return: bool
+
+    The function should return True if Pac-Man is touching a power pellet or a dot.
     """
     pass
 
@@ -24,6 +29,8 @@ def lose(power_pellet_active, touching_ghost):
     :param power_pellet_active: bool - does the player have an active power pellet?
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool
+
+    The function should return True if Pac-Man is touching a ghost and does not have a power pellet active.
     """
     pass
 
@@ -35,5 +42,7 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     :param power_pellet_active: bool - does the player have an active power pellet?
     :param touching_ghost:  bool - is the player touching a ghost?
     :return: bool
+
+    The function should return True if Pac-Man has eaten all of the dots and has not lost based on the parameters defined in part 3.
     """
     pass
