c709892e24f83b26bc9086e1487b6c6b13d51eff
Steve Bonds <sbonds@gmail.com>
2022-01-17 22:08:39 -0800

    Another nice list method.

--- python/chaitanas-colossal-coaster/list_methods.py
+++ python/chaitanas-colossal-coaster/list_methods.py
@@ -63,7 +63,7 @@ def how_many_namefellows(queue, person_name):
     :param person_name: str - name you wish to count or track.
     :return:  int - the number of times the name appears in the queue.
     """
-    pass
+    return queue.count(person_name)
 
 
 def remove_the_last_person(queue):
