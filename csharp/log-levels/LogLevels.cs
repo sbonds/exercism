9f0b5f31963b286facd6aadd879ef313bc395bab
Steve Bonds <sbonds@gmail.com>
2022-10-15 11:08:50 -0700

    string.Format works

--- csharp/log-levels/LogLevels.cs
+++ csharp/log-levels/LogLevels.cs
@@ -1,19 +1,26 @@
 using System;
+using System.Text.RegularExpressions;
 
 static class LogLine
 {
+    // Now you have two problems.
+    static string messageSplitRegex = @"^\[(INFO|WARNING|ERROR)\]:\s+(.*)$";
+
     public static string Message(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
+        // https://learn.microsoft.com/en-us/dotnet/api/system.text.regularexpressions.matchcollection?view=net-6.0
+        return Regex.Match(logLine,messageSplitRegex).Groups[2].Value.Trim();
     }
 
     public static string LogLevel(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
+        return Regex.Match(logLine,messageSplitRegex).Groups[1].Value.ToLower();
     }
 
     public static string Reformat(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
+        // https://stackoverflow.com/questions/63322784/c-sharp-how-to-treat-a-string-variable-as-interpolated-string
+        return string.Format("{0} ({1})",Message(logLine),LogLevel(logLine));
     }
+
 }
